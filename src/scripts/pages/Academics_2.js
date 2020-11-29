51e1c67d4b824b845452635de605692516f142d1
hannajones <jones.hanna.m@gmail.com>
2020-11-29 15:47:46 -0500

    set some things to 100%

--- src/scripts/pages/Academics_2.js
+++ src/scripts/pages/Academics_2.js
@@ -1,7 +1,7 @@
 import { html } from 'lit-html';
 
 const academics_2 = (html`
-    <div class="page page--academics" name="academics">
+    <div class="page page--academics page--academics_2" name="academics">
         <div class="card">
            <h2 class="title card__title card__title--academics">Current Research</h2>
             <div class="card__section">
