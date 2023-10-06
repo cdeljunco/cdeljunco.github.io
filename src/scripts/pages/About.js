8fb859e99a7a7b9204c2092fdf2a303205c46462
Cay del Junco <caydj@posteo.net>
2023-10-06 17:53:30 -0400

    minor edits & tweaks

--- src/scripts/pages/About.js
+++ src/scripts/pages/About.js
@@ -10,13 +10,13 @@ const about = (html`
 	I am a science librarian currently pursuing a Masters of Information in Library and Information Science at the University of Toronto. </p>           
 	    <p class="card__body card__body--about">
 
-               As a practicing librarian, my current interests lie in critical teaching methods for STEM librarianship; equitable models of open access scholarly publishing; and any kind of data analysis that keeps me connected with my computational roots (bibliometrics, collections analysis, etc.). As a researcher, I am broadly interested in the power, politics, and praxis of academic knowledge production and scholarly communication, especially in the natural sciences.</p>
+               As a practicing librarian, my current interests lie in critical teaching methods for STEM librarianship; equitable models of open access scholarly publishing; and any kind of data analysis that keeps me connected with my computational roots (bibliometrics, collections analysis, etc.). As a researcher, I am broadly interested in power & politics in academic knowledge production and scholarly communication, especially in the natural sciences.</p>
 
 <p class="card__body card__body--about"> 
-I was previously the STEM and Data Education Librarian at Wesleyan University. I trained as a computational chemist and physicist with <a class="card__body__link card__body__link--about" href="http://vaikuntanathan-group.uchicago.edu/" target="_blank">Suri Vaikuntanathan</a>, and did a postdoc in sociology with the <a class="card__body__link card__body__link--about" href="https://www.knowledgelab.org/" target="_blank">Knowledge Lab</a>, both at the University of Chicago.</p>
+From 2021-23 I was the STEM and Data Education Librarian at Wesleyan University. I trained as a computational chemist and physicist with <a class="card__body__link card__body__link--about" href="http://vaikuntanathan-group.uchicago.edu/" target="_blank">Suri Vaikuntanathan</a> (2014-20), and did a postdoc in sociology with the <a class="card__body__link card__body__link--about" href="https://www.knowledgelab.org/" target="_blank">Knowledge Lab</a> (2020-21), both at the University of Chicago.</p>
 
 <p class="card__body card__body--about"> 
-A small selection of my interests: techno music, labor organizing in higher education, swimming in lakes, anything cooperative, having good nails, cycling, and cooking. Check the <a class="card__body__link card__body__link--about" href="#links" target="_blank">links</a> page for some non-academic things I've written & made.
+A small selection of my interests: techno music, labor organizing in higher education, swimming in lakes, anything cooperative, good nails, cycling, and cooking. Check the <a class="card__body__link card__body__link--about" href="#links" target="_blank">links</a> page for some non-academic things I've written/edited/made.
 </p>
 <p class="card__body card__body--about">
 This website was inspired by <a class="card__body__link card__body__link--about" href="https://www.brutalistwebsites.com/" target="_blank">brutalist web design</a>, especially <a class="card__body__link card__body__link--about" href="https://crevv.com/" target="_blank">crevv.com</a>, and built and designed by my friend  <a class="card__body__link card__body__link--about" href="https://www.hanna-jones.com/projects" target="_blank">Hanna Jones</a>, fellow techno enthusiast and web developer.
@@ -24,15 +24,11 @@ This website was inspired by <a class="card__body__link card__body__link--about"
             <div class="contact onMobile">
                 <div class="contact__item">
                     <img class="contact__icon" src="./public/assets/images/mail.svg" alt="email icon">
-                    <a class="contact__link" href="mailto:clara.deljunco@posteo.net">Email</a>
+                    <a class="contact__link" href="mailto:caydj@posteo.net">Email</a>
                 </div>
                <!-- <div class="contact__item"> -->
                 <!--   <img class="contact__icon" src="./public/assets/images/file-text.svg" alt="email icon"> -->
                 <!--    <a class="contact__link" href="./files/cv-cdj.pdf" download target="_blank">CV</a> -->
-                <div class="contact__item">
-                    <img class="contact__icon" src="./public/assets/images/book-open.svg" alt="book icon">
-                    <a class="contact__link" href="https://scholar.google.com/citations?user=NyQ4unsAAAAJ&hl=en" target="_blank">Google Scholar</a>
-                </div>
             </div>
         </div>
     </div>
--- src/scripts/pages/Links.js
+++ src/scripts/pages/Links.js
@@ -6,22 +6,22 @@ const links = (html`
             <h2 class="title card__title">Links</h2>
             <div class="card__section">
                 <p class="card__body card__body--links">
-                   <a class="card__body__link" href="https://drive.google.com/file/d/1yqyZdaEDH9HQ98qkpcE-cA_eK7dMKwtY/view" target="_blank">Rave Ethics Zine</a>: Fun, safety, and ethical behavior -- the 3 rules of the rave. Send me an email if you can guess which part I contributed.
+                   <a class="card__body__link" href="https://drive.google.com/file/d/1yqyZdaEDH9HQ98qkpcE-cA_eK7dMKwtY/view" target="_blank">Rave Ethics Zine</a>: Fun, safety, and ethical behavior -- the 3 rules of the rave. I contributed to this zine that was edited by my dear friend Char Hilgers. 
                 </p>
 		</div>
 	     <div class="card__section">
                 <p class="card__body card__body--links">
-                   <a class="card__body__link" href="http://futureethics.net/" target="_blank">Future Ethics</a>: A label and zine born of Rave Ethics, run by my friends Catherine Hilgers (aka Ursula Xanadu) and Bee Hill. I made the website and Catherine maintains it.
+                   <a class="card__body__link" href="http://futureethics.net/" target="_blank">Future Ethics</a>: A label and zine born of Rave Ethics, run by my friends Char Hilgers and Bee Hill. I made the website and Char maintains it.
                 </p>            
 	   </div>
             <div class="card__section">
                 <p class="card__body card__body--links">
-                   <a class="card__body__link" href="http://dx.doi.org/10.6082/uchicago.2773" target="_blank">Burnout</a>: A collection of visual and written reflections on academia, travel, and climate change, edited by me and Mathilde Gerbelli-Gauthier.
+                   <a class="card__body__link" href="http://dx.doi.org/10.6082/uchicago.2773" target="_blank">Burnout</a>: A collection of visual and written reflections on academia, travel, and climate change, by some amazing friends, colleagues, roommates, comrades, and one stranger from the internet. Edited with <a class="card__body__link" href="https://mgerbelli.github.io/website/" target="_blank">Mathilde Gerbelli-Gauthier</a>.
                 </p>
 	   </div>
 	    <div class="card__section">
                 <p class="card__body card__body--links">
-                   <a class="card__body__link" href="https://www.insidehighered.com/views/2022/04/05/raise-racist-scientific-history-science-papers-opinion" target="_blank">A Weighty Footnote</a>: An opinion piece where I argue that history and politics belong in science papers, based on one of my experiences publishing in a physics journal.                
+                   <a class="card__body__link" href="https://www.insidehighered.com/views/2022/04/05/raise-racist-scientific-history-science-papers-opinion" target="_blank">A Weighty Footnote</a>: I wrote this piece for Inside Higher Ed in reaction to an experience I had trying to include historical context in a physics paper. In it, I argue that history and politics belong in science papers.                
 		   </p>
 	   </div>
 
